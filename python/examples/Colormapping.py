1d017496532bf5603f9a88cf7d2e0717d82c5cf3
Tornadiz <88659602+Tornadiz@users.noreply.github.com>
2021-08-10 14:36:56 +0200

    Update Colormapping.py

--- python/examples/Colormapping.py
+++ python/examples/Colormapping.py
@@ -77,13 +77,14 @@ def rotate_gradient(strip, img_gradient, locations, speed):
     col_map_old = []
     for k in range(0, LED_COUNT):
         col_map_old = colormap(strip, img_gradient, locations, k, bright)
+        print("col_map_new before: ", col_map_new)
         col_map_new = col_map_old
         col_map_new.append(col_map_new.pop(0))
+        print("col_map_new after: ", col_map_new)
         for en_counter, entry in enumerate(col_map_new):
                 col_map_new[en_counter] = col_map_old[en_counter]        
-        print("old: " + str(col_map_old[0]) + ", new: " + str(col_map_new[0]))
+#         print("old: " + str(col_map_old[0]) + ", new: " + str(col_map_new[0]))
         fade_gradient(strip, col_map_old, col_map_new, locations, speed*20)
-        col_map_old = colormap(strip, img_gradient, locations, k, bright)
 #                 illuminate_all_pos(color_map)
         time.sleep(0.5/(speed/100))
         
