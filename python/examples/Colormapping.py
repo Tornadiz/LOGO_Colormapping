61152dbdab6b7fb2a25b5d51b2a8e1a727f35038
Tornadiz <88659602+Tornadiz@users.noreply.github.com>
2021-08-10 12:16:00 +0200

    Update Colormapping.py

--- python/examples/Colormapping.py
+++ python/examples/Colormapping.py
@@ -26,7 +26,7 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 # gradient_li = ["gradient_logo.jpg"]
 gradient = "gradient_logo.jpg"
-speed = 200   # in %
+speed = 300   # in %
 locations = [(1147,918),(385,1139),(627,841),(819,1321),(1254,939),(364,145),(392,748),(96,1167),(703,1029),(516,173),(145,573),
                 (455,818),(281,258),(1237,1269),(555,1249),(86,296),(1242,228),(41,863),(453,387),(225,102),(32,785),
                 (462,819),(1241,89),(212,1388),(739,1100),(1116,1587),(683,944),(1163,1564),(894,1096),(1268,1529),
@@ -47,17 +47,21 @@ def colormap(strip, img_gradient, locations, shift, brightness):
         
     for j, (x, y) in enumerate(locations):
 #         print("img_gradient[x, y]: ", img_gradient[x, y])
-        color = Color(int(img_gradient[x, y][0]), int(img_gradient[x, y][1]), int(img_gradient[x, y][2]))
+        
+        B = int(img_gradient[x, y][0])
+        G = int(img_gradient[x, y][1])
+        R = int(img_gradient[x, y][2])
+        color = Color(R, G, B)
         
         current_LED_num = j + shift        
         
         if current_LED_num > LED_COUNT-1: 
             current_LED_num = current_LED_num % LED_COUNT
-        print("Point " + str(current_LED_num) + ": Color: " + str(color))
+#         print("Point " + str(current_LED_num) + ": Color: " + str(color))
 #         print("Point " + str(current_LED_num) + ": Color: " + str(color))
         
         strip.setPixelColor(current_LED_num, color)
-    strip.show()
+        strip.show()
     # theaterChase(strip, Color(127, 127, 127))  # White theater chase
         
 # gradient animation functions      
@@ -158,7 +162,7 @@ if __name__ == '__main__':
         
                   # theaterChase(strip, Color(127, 127, 127))  # White theater chase
 
-            print ('gradient_list(' + str(i) + ') now playing')
+#             print ('gradient_list(' + str(i) + ') now playing')
         
             rotate_gradient(strip, image_grad, locations, speed)
             
