40576cdc0dc7bfeee68253b46e94d940ae87d298
Tornadiz <88659602+Tornadiz@users.noreply.github.com>
2021-08-10 12:31:23 +0200

    Update Colormapping.py

--- python/examples/Colormapping.py
+++ python/examples/Colormapping.py
@@ -76,12 +76,12 @@ def rotate_gradient(strip, img_gradient, locations, speed):
         
         
 # # Define functions which animate LEDs in various ways.
-# def colorWipe(strip, color, wait_ms=50):
-#     """Wipe color across display a pixel at a time."""
-#     for i in range(strip.numPixels()):
-#         strip.setPixelColor(i, color)
-#         strip.show()
-#         time.sleep(wait_ms/1000.0)
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
 
 # def theaterChase(strip, color, wait_ms=50, iterations=10):
 #     """Movie theater light style chaser animation."""
